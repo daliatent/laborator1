d6250df3a6501e0e63a203c8977f6bcc9386fcb1
Dalia Tent <daliatent09@gmail.com>
2024-03-27 00:16:51 +0200

    Add project files.

---
+++ laborator1/Program.cs
@@ -0,0 +1,22 @@
+﻿
+/*Console.WriteLine("introduceti latimea");
+int latime = int.Parse(Console.ReadLine());
+Console.WriteLine("introduceti inaltimea");
+int inaltime = int.Parse(Console.ReadLine());
+Console.WriteLine("introduceti adancimea");
+int adancime = int.Parse(Console.ReadLine());
+int volum = latime * inaltime * adancime;
+
+Console.WriteLine("Volumul paralelipipedului:");
+Console.WriteLine(volum);
+*/
+Console.WriteLine("introduceti x");
+int x = int.Parse(Console.ReadLine());
+if(x> 0)
+{
+    Console.WriteLine("numarul e pozitiv");
+}
+else
+{
+    Console.WriteLine("numarul e negativ");
+}
